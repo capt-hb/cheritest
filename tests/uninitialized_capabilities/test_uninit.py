65855a98b8e6c486c9d55d9b3324ac6abd3ee540
Sander Huyghebaert <sanderhuyghebaert@yahoo.com>
2020-02-07 13:55:04 +0100

    uninit test adjusted to use uninit field of capabilities

--- tests/uninitialized_capabilities/test_uninit.py
+++ tests/uninitialized_capabilities/test_uninit.py
@@ -3,8 +3,8 @@ from beritest_tools import BaseBERITestCase
 class test_uninit(BaseBERITestCase):
     def test_uninit_is_zero_of_default_cap(self):
         '''Test that the default capability has the uninit bit set to 0'''
-        assert self.MIPS.a0 == 0
+        assert self.MIPS.c1.uninit == 0
 
     def test_uninit_non_zero(self):
         '''Test that the uninit bit is non-zero after making a capability uninitialized'''
-        assert self.MIPS.a1 == 1
+        assert self.MIPS.c2.uninit == 1
--- tests/uninitialized_capabilities/test_uninit.s
+++ tests/uninitialized_capabilities/test_uninit.s
@@ -5,7 +5,5 @@
 
 BEGIN_TEST
 	cgetdefault $c1
-	cgetuninit $a0, $c1
 	cuninit $c2, $c1
-	cgetuninit $a1, $c2
 END_TEST

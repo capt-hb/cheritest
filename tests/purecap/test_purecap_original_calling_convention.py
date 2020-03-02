65d0a9f0926e684190c95a27fa05412e12c29832
Sander Huyghebaert <sanderhuyghebaert@yahoo.com>
2020-03-02 11:18:47 +0100

    test that value stored in first return register () is the same as the argument

--- tests/purecap/test_purecap_original_calling_convention.py
+++ tests/purecap/test_purecap_original_calling_convention.py
@@ -3,3 +3,6 @@ from beritest_tools import BaseBERITestCase
 class test_purecap_original_calling_convention(BaseBERITestCase):
     def test_first_argument_register_contains_100(self):
         assert self.MIPS.a0 == 100
+
+    def test_result_of_something_is_100(self):
+        assert self.MIPS.v0 == 100

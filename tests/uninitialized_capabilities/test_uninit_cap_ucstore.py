0277cc0ecbccd9db8c5eba74b957be3d6ab15196
Sander Huyghebaert <sanderhuyghebaert@yahoo.com>
2020-03-16 14:30:45 +0100

    updated ucstore test files for uninit cap where reading can only happen between [cursor,end]

--- tests/uninitialized_capabilities/test_uninit_cap_ucstore.py
+++ tests/uninitialized_capabilities/test_uninit_cap_ucstore.py
@@ -1,18 +1,16 @@
 from beritest_tools import BaseBERITestCase
 
 class test_uninit_cap_ucstore(BaseBERITestCase):
-    EXPECTED_EXCEPTIONS = 2
-
-    def test_writing_then_reading_succeeds(self):
-        '''Writing on cursor with ucstore works and then reading on offset -1 results
-           in the same value'''
-        assert self.MIPS.a5 == 42
+    EXPECTED_EXCEPTIONS = 1
 
     def test_offset_is_incremented_after_ucstore(self):
         '''Offset after writing gets incremented'''
-        assert self.MIPS.a0 == 0
-        assert self.MIPS.a1 == 1
-        assert self.MIPS.a2 == 2
-        assert self.MIPS.a3 == 3 
-        assert self.MIPS.a4 == 3 
-        assert self.MIPS.a6 == 50
+        assert self.MIPS.a0 == 15
+        assert self.MIPS.a1 == 10
+        assert self.MIPS.a2 == 42
+        assert self.MIPS.a3 == 100
+        assert self.MIPS.a4 == 420 
+        assert self.MIPS.s0 == 10
+        assert self.MIPS.s1 == 42
+        assert self.MIPS.s2 == 100
+        assert self.MIPS.s3 == 420 
--- tests/uninitialized_capabilities/test_uninit_cap_ucstore.s
+++ tests/uninitialized_capabilities/test_uninit_cap_ucstore.s
@@ -15,38 +15,42 @@ BEGIN_TEST
                 cgetdefault $c1
                 dla $t0, data
                 csetoffset $c1, $c1, $t0
-                dli $t0, 8
-                csetbounds $c1, $c1, $t0
+                csetboundsimm $c1, $c1, 15
 
-		# Make $c1 uninitialized
+		# Setup $c1 as uninit cap
+		cgetlen $t0, $c1
+		csetoffset $c1, $c1, $t0
 		cuninit $c1, $c1
 		cgetoffset $a0, $c1
                 
+		# reading underneath cursor should throw an error
+                clb $a0, $zero, -2($c1)
 
-		# Store 42 on cursor, store offset and value at offset -1 of cap into registers
-		dla $t0, data
-                dli     $t1, 42
-                ucsb    $c1, $t1, 0($c1)
-		cgetoffset $a1, $c1
-                clb     $a5, $zero, -1($c1)
-
-                dli     $t1, 99
-                ucsb    $c1, $t1, 0($c1)
-		cgetoffset $a2, $c1
+		# Store 10 as a byte on cursor
                 dli     $t1, 10
-		ucsb $c1, $t1, 0($c1)
-		cgetoffset $a3, $c1
+                ucsb    $c1, $t1, 0($c1)
+		clb $s0, $zero, 0($c1)
 
-                dli     $t1, 50
-		ucsb $c1, $t1, -2($c1)
-		cgetoffset $a4, $c1
+		# Store 42 as a half word on cursor
+                dli     $t1, 42
+                ucsh    $c1, $t1, 0($c1)
+		clh $s1, $zero, 0($c1)
 
-		# will throw an exception, reading on cursor before writing!
-                clb     $a6, $zero, 0($c1)
-		# will throw an exception, reading above cursor!
-                clb     $a6, $zero, 1($c1)
+		# Store 100 as a word on cursor
+                dli     $t1, 100
+                ucsw    $c1, $t1, 0($c1)
+		clw $s2, $zero, 0($c1)
 
-                clb     $a6, $zero, -2($c1)
+		# Store 420 as a double word on cursor
+                dli     $t1, 420
+                ucsd    $c1, $t1, 0($c1)
+		cld $s3, $zero, 0($c1)
+
+		# Load values stored into registers
+		clb $a1, $zero, 14($c1)
+		clh $a2, $zero, 12($c1)
+		clw $a3, $zero, 8($c1)
+		cld $a4, $zero, 0($c1)
 END_TEST
 
                 .data
@@ -54,8 +58,12 @@ END_TEST
                 .dword  0x0
                 .dword  0x0
                 .dword  0x0
-underflow:      .dword  0x0123456789abcdef
 data:           .dword  0x0123456789abcdef
-overflow:       .dword  0x0123456789abcdef      # check for overflow
-                .dword  0x0
-                .dword  0x0
+		.dword  0x0123456789abcdef
+		.dword  0x0123456789abcdef
+		.dword  0x0123456789abcdef
+		.dword  0x0123456789abcdef
+		.dword  0x0123456789abcdef
+		.dword  0x0123456789abcdef
+		.dword  0x0123456789abcdef
+		.dword  0x0123456789abcdef

ec8cc8acbc1fd46ab8f1c62f3536d19228266aaa
Sander Huyghebaert <sanderhuyghebaert@yahoo.com>
2020-03-16 13:41:08 +0100

    updated offset tests after changing uninit cap to conform with the stack growing downwards

--- tests/uninitialized_capabilities/test_uninit_cap_csetoffset.py
+++ tests/uninitialized_capabilities/test_uninit_cap_csetoffset.py
@@ -3,15 +3,14 @@ from beritest_tools import BaseBERITestCase
 class test_uninit_cap_csetoffset(BaseBERITestCase):
     EXPECTED_EXCEPTIONS = 1
 
-    def test_setting_offset_lower_than_cursor(self):
-        '''Setting the offset lower than the offset is currently at should succeed'''
+    def test_offset_correctly_updated_after_store(self):
+        '''test that the cursor has been decremented with the correct size'''
         assert self.MIPS.a1 == 1
-        assert self.MIPS.a2 == 0
-        assert self.MIPS.a3 == 1
-        assert self.MIPS.a4 == 0
-        assert self.MIPS.a5 == 1
-        assert self.MIPS.a6 == 0
+        assert self.MIPS.a2 == 2
+        assert self.MIPS.a3 == 4
+        assert self.MIPS.a4 == 8
+        assert self.MIPS.a5 == 15
 
-    def test_set_offset_higher_once_cap_is_completely_written_to(self):
-        '''Should be possible to set offset outside of bounds when cap is completely written to'''
-        assert self.MIPS.a7 == 4
+    def test_set_offset_outside_of_bounds(self):
+        '''Should be possible to set offset outside of bounds'''
+        assert self.MIPS.a6 == 115
--- tests/uninitialized_capabilities/test_uninit_cap_csetoffset.s
+++ tests/uninitialized_capabilities/test_uninit_cap_csetoffset.s
@@ -11,41 +11,54 @@ BEGIN_TEST
 	cgetdefault $c1
 	dla $t0, data
 	csetoffset $c1, $c1, $t0
-	dli $t0, 2
+	dli $t0, 15 # capability to store a byte, halfword, word and double word
 	csetbounds $c1, $c1, $t0
 
+	# place cursor at the end of the capability
+	cgetlen $t0, $c1
+	csetoffset $c1, $c1, $t0
+	cincoffsetimm $c1, $c1, -1 # allows storing a byte (needed for instrs below)
 	cuninit $c1, $c1
-	dli $t0, 10
-	cincoffset $c1, $c1, $t0 # inc offset higher than cursor should throw an error
 
-	dli     $t1, 42
-	ucsb    $c1, $t1, 0($c1)
-	dli $t1, 0
-	cgetoffset $a1, $c1
-	csetoffset $c1, $c1, $t1
-	cgetoffset $a2, $c1
+	dli $t0, -10
+	cincoffset $c1, $c1, $t0 # inc offset lower than cursor should throw an error
 
-	dli     $t1, 42
+	# store 10 as a byte on the cursor of the uninit cap
+	dli     $t1, 10 
+	cgetoffset $s0, $c1 # store the offset 
 	ucsb    $c1, $t1, 0($c1)
-	dli $t1, -1
-	cgetoffset $a3, $c1
-	cincoffset $c1, $c1, $t1
-	cgetoffset $a4, $c1
+	cgetoffset $s1, $c1
+	sub $a1, $s0, $s1 # a byte was written so $a1 should contain 1 (decr of cursor after writing)
 
+	# store 42 as a halfword on the cursor of the uninit cap
 	dli     $t1, 42
-	ucsb    $c1, $t1, 0($c1)
+	cgetoffset $s0, $c1 # store the offset 
+	ucsh $c1, $t1, 0($c1)
+	cgetoffset $s1, $c1
+	sub $a2, $s0, $s1 # halfword was written, so $a2 should contain 2
+
+	# store 100 as a word on the cursor of the uninit cap
+	dli $t1, 100
+	cgetoffset $s0, $c1 # store the offset 
+	ucsw $c1, $t1, 0($c1)
+	cgetoffset $s1, $c1
+	sub $a3, $s0, $s1 # halfword was written, so $a2 should contain 4
+
+	# store 420 as a double word on the cursor of the uninit cap
+	dli     $t1, 420
+	cgetoffset $s0, $c1 # store the offset 
+	ucsd $c1, $t1, 0($c1)
+	cgetoffset $s1, $c1
+	sub $a4, $s0, $s1 # halfword was written, so $a2 should contain 4
+
+	cgetlen $t0, $c1
+	csetoffset $c1, $c1, $t0
 	cgetoffset $a5, $c1
-	cincoffsetimm $c1, $c1, -1
+	cincoffsetimm $c1, $c1, 100
 	cgetoffset $a6, $c1
-
-	# completely write capability
-	dli     $t1, 42
-	ucsb    $c1, $t1, 0($c1)
-	ucsb    $c1, $t1, 0($c1)
-	cincoffsetimm $c1, $c1, 2 # set offset higher than cap bounds
-	cgetoffset $a7, $c1
 END_TEST
 	.data
 	.align 5
 
 data:	.dword  0x0123456789abcdef
+	.dword  0x0123456789abcdef

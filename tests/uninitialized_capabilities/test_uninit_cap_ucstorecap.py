b16f7edaa0a4cf9a02b9fc6f89ccec67d4b6e56e
Sander Huyghebaert <sanderhuyghebaert@yahoo.com>
2020-05-19 17:37:11 +0200

    tests for UCSC instruction

---
+++ tests/uninitialized_capabilities/test_uninit_cap_ucstorecap.py
@@ -0,0 +1,13 @@
+from beritest_tools import BaseBERITestCase
+
+class test_uninit_cap_ucstorecap(BaseBERITestCase):
+    EXPECTED_EXCEPTIONS = 0
+
+    def test_offset_is_decremented_after_ucstorecap(self):
+        '''Offset after writing gets incremented'''
+        assert self.MIPS.a0 == 80
+        assert self.MIPS.a1 == 76
+        assert self.MIPS.a2 == 80 
+        assert self.MIPS.a3 == 80 
+        assert self.MIPS.a4 == 80 
+        assert self.MIPS.s1 == 32
---
+++ tests/uninitialized_capabilities/test_uninit_cap_ucstorecap.s
@@ -0,0 +1,66 @@
+.set mips64
+.set noreorder
+.set nobopt
+.include "macros.s"
+
+#
+# Test ucstorecap instruction on uninit cap
+#
+
+BEGIN_TEST
+                cgetdefault $c1
+                dla $t0, data
+                csetoffset $c1, $c1, $t0
+		dli $t1, 80
+                csetbounds $c1, $c1, $t1
+
+		# Setup $c1 as uninit cap
+		csetoffset $c1, $c1, $t1
+		cgetnull $c13
+		cuninit $c1, $c1
+		cgetoffset $a0, $c1
+
+		ucsw $c1, $t1, 0($c1)
+		cgetoffset $a1, $c1
+		clw $a2, $zero, 0($c1)
+
+		# Create capability pointing to word containing contents of $t1
+		csetbounds $c2, $c1, 4
+
+		# store $c2
+		ucsw $c1, $t1, 0($c1) # needed for 32-bit alignment of caps
+		cgetoffset $s0, $c1
+		ucsc $c1, $c2, 0($c1)
+		cgetoffset $s1, $c1
+		sub $s1, $s0, $s1
+
+		clc $c3, $zero, 0($c1)
+		clw $a3, $zero, 0($c2)
+		clw $a4, $zero, 0($c3)
+END_TEST
+
+                .data
+                .align  5               # 256-bit align
+                .dword  0x0
+                .dword  0x0
+                .dword  0x0
+data:           .dword  0x0123456789abcdef
+		.dword  0x0123456789abcdef
+		.dword  0x0123456789abcdef
+		.dword  0x0123456789abcdef
+		.dword  0x0123456789abcdef
+		.dword  0x0123456789abcdef
+		.dword  0x0123456789abcdef
+		.dword  0x0123456789abcdef
+		.dword  0x0123456789abcdef
+		.dword  0x0123456789abcdef
+		.dword  0x0123456789abcdef
+		.dword  0x0123456789abcdef
+		.dword  0x0123456789abcdef
+		.dword  0x0123456789abcdef
+		.dword  0x0123456789abcdef
+		.dword  0x0123456789abcdef
+		.dword  0x0123456789abcdef
+		.dword  0x0123456789abcdef
+		.dword  0x0123456789abcdef
+		.dword  0x0123456789abcdef

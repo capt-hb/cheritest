b67b800d07b053bb6da26ad1c9a8053f657b8262
Sander Huyghebaert <sanderhuyghebaert@yahoo.com>
2020-05-26 15:02:51 +0200

    improved test for ucstore instr to ensure that the offset gets decremented

--- tests/uninitialized_capabilities/test_uninit_cap_ucstore.py
+++ tests/uninitialized_capabilities/test_uninit_cap_ucstore.py
@@ -3,9 +3,16 @@ from beritest_tools import BaseBERITestCase
 class test_uninit_cap_ucstore(BaseBERITestCase):
     EXPECTED_EXCEPTIONS = 1
 
-    def test_offset_is_incremented_after_ucstore(self):
-        '''Offset after writing gets incremented'''
+    def test_offset_is_decremented_after_ucstore(self):
+        '''Offset after writing gets decremented'''
         assert self.MIPS.a0 == 15
+        assert self.MIPS.a0 - self.MIPS.t2 == 1
+        assert self.MIPS.t2 - self.MIPS.t3 == 2
+        assert self.MIPS.t3 - self.MIPS.v0 == 4
+        assert self.MIPS.v0 - self.MIPS.v1 == 8
+
+    def test_stored_values_are_correct(self):
+        '''test stored values can be loaded and have the same value'''
         assert self.MIPS.a1 == 10
         assert self.MIPS.a2 == 42
         assert self.MIPS.a3 == 100
--- tests/uninitialized_capabilities/test_uninit_cap_ucstore.s
+++ tests/uninitialized_capabilities/test_uninit_cap_ucstore.s
@@ -29,21 +29,25 @@ BEGIN_TEST
 		# Store 10 as a byte on cursor
                 dli     $t1, 10
                 ucsb    $c1, $t1, 0($c1)
+		cgetoffset $t2, $c1
 		clb $s0, $zero, 0($c1)
 
 		# Store 42 as a half word on cursor
                 dli     $t1, 42
                 ucsh    $c1, $t1, 0($c1)
+		cgetoffset $t3, $c1
 		clh $s1, $zero, 0($c1)
 
 		# Store 100 as a word on cursor
                 dli     $t1, 100
                 ucsw    $c1, $t1, 0($c1)
+		cgetoffset $v0, $c1
 		clw $s2, $zero, 0($c1)
 
 		# Store 420 as a double word on cursor
                 dli     $t1, 420
                 ucsd    $c1, $t1, 0($c1)
+		cgetoffset $v1, $c1
 		cld $s3, $zero, 0($c1)
 
 		# Load values stored into registers

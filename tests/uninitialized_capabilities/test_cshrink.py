266bb9171e2c7b054c10672fc007b55f691042ed
Sander Huyghebaert <sanderhuyghebaert@yahoo.com>
2020-05-27 10:12:02 +0200

    updated cshrink tests

--- tests/uninitialized_capabilities/test_cshrink.py
+++ tests/uninitialized_capabilities/test_cshrink.py
@@ -6,9 +6,10 @@ class test_cshrink(BaseBERITestCase):
     def test_cshrink_lowers_end(self):
         '''Test that lowering the end of a capability works'''
         assert self.MIPS.a0 == 10
-        assert self.MIPS.a1 == 8
+        assert self.MIPS.a1 == 9
         assert self.MIPS.a1 < self.MIPS.a0
+        assert self.MIPS.a2 == 1
 
     def test_cshrink_with_cursor_at_end_does_nothing(self):
         '''Test that using cshrink with a capability for which cursor=end does not change end'''
-        assert self.MIPS.a0 == self.MIPS.a2
+        assert self.MIPS.a0 == self.MIPS.a3
--- tests/uninitialized_capabilities/test_cshrink.s
+++ tests/uninitialized_capabilities/test_cshrink.s
@@ -9,16 +9,20 @@ BEGIN_TEST
 	csetoffset $c1, $c1, $t0
 	csetboundsimm $c1, $c1, 10
 	cincoffsetimm $c1, $c1, 8
-	cshrink $c2, $c1
+	cshrink $c2, $c1 		# length of $c2 = offset + 1 (= 9)
 	cgetlen $a0, $c1
 	cgetlen $a1, $c2
 
 	csetoffset $c1, $c1, $a0
-	cshrink $c3, $c1
-	cgetlen $a2, $c3
+	cshrink $c3, $c1 		# error: shrinking with offset out of bounds shouldn't work
 
-	cincoffsetimm $c4, $c1, 1 	# set cursor just outside bounds
-	cshrink $c4, $c4		# should throw error, cursor > end!
+	cincoffsetimm $c4, $c1, -10
+	cshrink $c4, $c4		
+	cgetlen $a2, $c4
+
+	cincoffsetimm $c5, $c1, -1 	# cursor will now be on end (= length - 1)
+	cshrink $c5, $c5
+	cgetlen $a3, $c5
 END_TEST
 
                 .data

cadf5e345066bca734f0ffa5b262d547ffc0d3ad
Sander Huyghebaert <sanderhuyghebaert@yahoo.com>
2020-05-28 09:22:32 +0200

    manually modified slow_factorial calling convention to uninit cc

--- tests/purecap/test_purecap_uninit_cc_slow_factorial.s
+++ tests/purecap/test_purecap_uninit_cc_slow_factorial.s
@@ -13,22 +13,18 @@ product:                                # @product
 	.set	nomacro
 	.set	noat
 # %bb.0:                                # %entry
-	cincoffset	$c11, $c11, -32
 	.cfi_def_cfa_offset 32
                                         # kill: def $a1 killed $a1 killed $a1_64
                                         # kill: def $a0 killed $a0 killed $a0_64
-	cincoffset	$c1, $c11, 28
-	csetbounds	$c1, $c1, 4
-	cincoffset	$c2, $c11, 24
-	csetbounds	$c2, $c2, 4
-	csw	$4, $zero, 0($c1)
-	csw	$5, $zero, 0($c2)
+	ucsw	$c11, $4, 0($c11)
+	csetbounds $c1, $c11, 4
+	ucsw	$c11, $5, 0($c11)
+	csetbounds $c2, $c11, 4
 	clw	$1, $zero, 0($c1)
 	clw	$2, $zero, 0($c2)
 	mult	$1, $2
 	mflo	$1
 	sll	$2, $1, 0
-	cincoffset	$c11, $c11, 32
 	cjr	$c17
 	nop
 	.set	at
@@ -54,33 +50,34 @@ factorial:                              # @factorial
 	.set	nomacro
 	.set	noat
 # %bb.0:                                # %entry
-	cincoffset	$c11, $c11, -192
 	.cfi_def_cfa_offset 192
-	csc	$c17, $zero, 160($c11)  # 32-byte Folded Spill
+	ucsc	$c11, $c17, 0($c11)  # 32-byte Folded Spill
 	.cfi_offset 89, -32
 	lui	$1, %pcrel_hi(_CHERI_CAPABILITY_TABLE_-8)
 	daddiu	$1, $1, %pcrel_lo(_CHERI_CAPABILITY_TABLE_-4)
 	cgetpccincoffset	$c1, $1
                                         # kill: def $a0 killed $a0 killed $a0_64
-	cincoffset	$c2, $c11, 156
-	csetbounds	$c2, $c2, 4
-	cincoffset	$c3, $c11, 152
-	csetbounds	$c3, $c3, 4
-	cincoffset	$c4, $c11, 148
-	csetbounds	$c4, $c4, 4
-	csw	$4, $zero, 0($c2)
+	ucsw	$c11, $4, 0($c11)
+	csetbounds	$c2, $c11, 4
 	addiu	$2, $zero, 1
-	csw	$2, $zero, 0($c3)
+	ucsw	$c11, $2, 0($c11)
+	csetbounds	$c3, $c11, 4
 	clw	$2, $zero, 0($c2)
-	csw	$2, $zero, 0($c4)
-	csc	$c1, $zero, 96($c11)    # 32-byte Folded Spill
-	csc	$c3, $zero, 64($c11)    # 32-byte Folded Spill
-	csc	$c4, $zero, 32($c11)    # 32-byte Folded Spill
+	ucsw	$c11, $2, 0($c11)
+	csetbounds	$c4, $c11, 4
+
+	ucsw	$c11, $zero, 0($c11)
+	ucsd	$c11, $zero, 0($c11)
+	ucsd	$c11, $zero, 0($c11)
+
+	ucsc	$c11, $c1, 0($c11)    # 32-byte Folded Spill
+	ucsc	$c11, $c3, 0($c11)    # 32-byte Folded Spill
+	ucsc	$c11, $c4, 0($c11)    # 32-byte Folded Spill
 	b	.LBB1_1
 	nop
 .LBB1_1:                                # %for.cond
                                         # =>This Inner Loop Header: Depth=1
-	clc	$c1, $zero, 32($c11)    # 32-byte Folded Reload
+	clc	$c1, $zero, 0($c11)    # 32-byte Folded Reload
 	clw	$1, $zero, 0($c1)
 	slti	$1, $1, 2
 	bnez	$1, .LBB1_5
@@ -91,32 +88,39 @@ factorial:                              # @factorial
 	nop
 .LBB1_3:                                # %for.body
                                         #   in Loop: Header=BB1_1 Depth=1
-	clc	$c1, $zero, 64($c11)    # 32-byte Folded Reload
+	clc	$c1, $zero, 32($c11)    # 32-byte Folded Reload
 	clw	$4, $zero, 0($c1)
-	clc	$c2, $zero, 32($c11)    # 32-byte Folded Reload
+	clc	$c2, $zero, 0($c11)    # 32-byte Folded Reload
 	clw	$5, $zero, 0($c2)
-	clc	$c3, $zero, 96($c11)    # 32-byte Folded Reload
+	clc	$c3, $zero, 64($c11)    # 32-byte Folded Reload
 	clcbi	$c12, %capcall20(product)($c3)
 	cgetnull	$c13
+	# CC: pre call
+	cmove $c21, $c11
+	cshrink $c11, $c11, 0
+	cuninit $c11, $c11
 	cjalr	$c12, $c17
 	nop
-	clc	$c1, $zero, 64($c11)    # 32-byte Folded Reload
-	csw	$2, $zero, 0($c1)
+	# CC: post call
+	cmove $c11, $c21
+	clc	$c1, $zero, 32($c11)    # 32-byte Folded Reload
+	cincoffset $c1, $c1, 4
+	ucsw	$c1, $2, 0($c1)
 	b	.LBB1_4
 	nop
 .LBB1_4:                                # %for.inc
                                         #   in Loop: Header=BB1_1 Depth=1
-	clc	$c1, $zero, 32($c11)    # 32-byte Folded Reload
+	clc	$c1, $zero, 0($c11)    # 32-byte Folded Reload
 	clw	$1, $zero, 0($c1)
 	addiu	$1, $1, -1
-	csw	$1, $zero, 0($c1)
+	cincoffset $c1, $c1, 4
+	ucsw	$c1, $1, 0($c1)
 	b	.LBB1_1
 	nop
 .LBB1_5:                                # %for.end
-	clc	$c1, $zero, 64($c11)    # 32-byte Folded Reload
+	clc	$c1, $zero, 32($c11)    # 32-byte Folded Reload
 	clw	$2, $zero, 0($c1)
-	clc	$c17, $zero, 160($c11)  # 32-byte Folded Reload
-	cincoffset	$c11, $c11, 192
+	clc	$c17, $zero, 128($c11)  # 32-byte Folded Reload
 	cjr	$c17
 	nop
 	.set	at
@@ -142,34 +146,33 @@ sum:                                    # @sum
 	.set	nomacro
 	.set	noat
 # %bb.0:                                # %entry
-	cincoffset	$c11, $c11, -224
 	.cfi_def_cfa_offset 224
                                         # kill: def $a0 killed $a0 killed $a0_64
-	cincoffset	$c1, $c11, 192
-	csetbounds	$c1, $c1, 32
-	cincoffset	$c2, $c11, 188
-	csetbounds	$c2, $c2, 4
-	cincoffset	$c4, $c11, 184
-	csetbounds	$c4, $c4, 4
-	cincoffset	$c5, $c11, 180
-	csetbounds	$c5, $c5, 4
-	csc	$c3, $zero, 0($c1)
-	csw	$4, $zero, 0($c2)
+	ucsc	$c11, $c3, 0($c11)
+	csetbounds $c1, $c11, 32
+	ucsw	$c11, $4, 0($c11)
+	csetbounds $c2, $c11, 4
 	addiu	$1, $zero, 0
-	csw	$zero, $zero, 0($c4)
-	csw	$zero, $zero, 0($c5)
-	csw	$1, $zero, 176($c11)    # 4-byte Folded Spill
-	csc	$c1, $zero, 128($c11)   # 32-byte Folded Spill
-	csc	$c2, $zero, 96($c11)    # 32-byte Folded Spill
-	csc	$c4, $zero, 64($c11)    # 32-byte Folded Spill
-	csc	$c5, $zero, 32($c11)    # 32-byte Folded Spill
+	ucsw	$c11, $zero, 0($c11)
+	csetbounds $c4, $c11, 4
+	ucsw	$c11, $zero, 0($c11)
+	csetbounds $c5, $c11, 4
+	ucsw	$c11, $1, 0($c11)    # 4-byte Folded Spill
+
+	ucsd $c11, $zero, 0($c11)
+	ucsd $c11, $zero, 0($c11)
+
+	ucsc	$c11, $c1, 0($c11)   # 32-byte Folded Spill
+	ucsc	$c11, $c2, 0($c11)    # 32-byte Folded Spill
+	ucsc	$c11, $c4, 0($c11)    # 32-byte Folded Spill
+	ucsc	$c11, $c5, 0($c11)    # 32-byte Folded Spill
 	b	.LBB2_1
 	nop
 .LBB2_1:                                # %for.cond
                                         # =>This Inner Loop Header: Depth=1
-	clc	$c1, $zero, 32($c11)    # 32-byte Folded Reload
+	clc	$c1, $zero, 0($c11)    # 32-byte Folded Reload
 	clw	$1, $zero, 0($c1)
-	clc	$c2, $zero, 96($c11)    # 32-byte Folded Reload
+	clc	$c2, $zero, 64($c11)    # 32-byte Folded Reload
 	clw	$2, $zero, 0($c2)
 	slt	$1, $1, $2
 	beqz	$1, .LBB2_5
@@ -180,30 +183,31 @@ sum:                                    # @sum
 	nop
 .LBB2_3:                                # %for.body
                                         #   in Loop: Header=BB2_1 Depth=1
-	clc	$c1, $zero, 128($c11)   # 32-byte Folded Reload
+	clc	$c1, $zero, 96($c11)   # 32-byte Folded Reload
 	clc	$c2, $zero, 0($c1)
-	clc	$c3, $zero, 32($c11)    # 32-byte Folded Reload
+	clc	$c3, $zero, 0($c11)    # 32-byte Folded Reload
 	clw	$1, $zero, 0($c3)
 	dsll	$1, $1, 2
 	clw	$2, $1, 0($c2)
-	clc	$c2, $zero, 64($c11)    # 32-byte Folded Reload
+	clc	$c2, $zero, 32($c11)    # 32-byte Folded Reload
 	clw	$3, $zero, 0($c2)
 	addu	$2, $3, $2
-	csw	$2, $zero, 0($c2)
+	cincoffset $c2, $c2, 4
+	ucsw	$c2, $2, 0($c2)
 	b	.LBB2_4
 	nop
 .LBB2_4:                                # %for.inc
                                         #   in Loop: Header=BB2_1 Depth=1
-	clc	$c1, $zero, 32($c11)    # 32-byte Folded Reload
+	clc	$c1, $zero, 0($c11)    # 32-byte Folded Reload
 	clw	$1, $zero, 0($c1)
 	addiu	$1, $1, 1
-	csw	$1, $zero, 0($c1)
+	cincoffset $c1, $c1, 4
+	ucsw	$c1, $1, 0($c1)
 	b	.LBB2_1
 	nop
 .LBB2_5:                                # %for.end
-	clc	$c1, $zero, 64($c11)    # 32-byte Folded Reload
+	clc	$c1, $zero, 32($c11)    # 32-byte Folded Reload
 	clw	$2, $zero, 0($c1)
-	cincoffset	$c11, $c11, 224
 	cjr	$c17
 	nop
 	.set	at
@@ -229,35 +233,40 @@ sumFactorials:                          # @sumFactorials
 	.set	nomacro
 	.set	noat
 # %bb.0:                                # %entry
-	cincoffset	$c11, $c11, -224
 	.cfi_def_cfa_offset 224
-	csc	$c17, $zero, 192($c11)  # 32-byte Folded Spill
+	ucsc	$c11, $c17, 0($c11)  # 32-byte Folded Spill
 	.cfi_offset 89, -32
 	lui	$1, %pcrel_hi(_CHERI_CAPABILITY_TABLE_-8)
 	daddiu	$1, $1, %pcrel_lo(_CHERI_CAPABILITY_TABLE_-4)
 	cgetpccincoffset	$c1, $1
-	cincoffset	$c2, $c11, 188
-	csetbounds	$c2, $c2, 4
-	cincoffset	$c3, $c11, 172
-	csetbounds	$c3, $c3, 16
-	cincoffset	$c4, $c11, 168
-	csetbounds	$c4, $c4, 4
 	addiu	$2, $zero, 4
-	csw	$2, $zero, 0($c2)
+	ucsw $c11, $2, 0($c11)
+	csetbounds $c2, $c11, 4
 	addiu	$2, $zero, 1
-	csw	$2, $zero, 0($c3)
-	csw	$2, $zero, 0($c4)
-	csc	$c1, $zero, 128($c11)   # 32-byte Folded Spill
-	csc	$c2, $zero, 96($c11)    # 32-byte Folded Spill
-	csc	$c3, $zero, 64($c11)    # 32-byte Folded Spill
-	csc	$c4, $zero, 32($c11)    # 32-byte Folded Spill
+
+	# Create array of 4 words
+	ucsw $c11, $zero, 0($c11)
+	ucsw $c11, $zero, 0($c11)
+	ucsw $c11, $zero, 0($c11)
+	ucsw $c11, $2, 0($c11)
+	csetbounds $c3, $c11, 16
+
+	ucsw $c11, $2, 0($c11)
+	csetbounds $c4, $c11, 4
+
+	ucsd $c11, $zero, 0($c11)
+
+	ucsc	$c11, $c1, 0($c11)   # 32-byte Folded Spill
+	ucsc	$c11, $c2, 0($c11)    # 32-byte Folded Spill
+	ucsc	$c11, $c3, 0($c11)    # 32-byte Folded Spill
+	ucsc	$c11, $c4, 0($c11)    # 32-byte Folded Spill
 	b	.LBB3_1
 	nop
 .LBB3_1:                                # %for.cond
                                         # =>This Inner Loop Header: Depth=1
-	clc	$c1, $zero, 32($c11)    # 32-byte Folded Reload
+	clc	$c1, $zero, 0($c11)    # 32-byte Folded Reload
 	clw	$1, $zero, 0($c1)
-	clc	$c2, $zero, 96($c11)    # 32-byte Folded Reload
+	clc	$c2, $zero, 64($c11)    # 32-byte Folded Reload
 	clw	$2, $zero, 0($c2)
 	slt	$1, $1, $2
 	beqz	$1, .LBB3_5
@@ -268,40 +277,54 @@ sumFactorials:                          # @sumFactorials
 	nop
 .LBB3_3:                                # %for.body
                                         #   in Loop: Header=BB3_1 Depth=1
-	clc	$c1, $zero, 32($c11)    # 32-byte Folded Reload
+	clc	$c1, $zero, 0($c11)    # 32-byte Folded Reload
 	clw	$4, $zero, 0($c1)
-	clc	$c2, $zero, 128($c11)   # 32-byte Folded Reload
+	clc	$c2, $zero, 96($c11)   # 32-byte Folded Reload
 	clcbi	$c12, %capcall20(factorial)($c2)
 	cgetnull	$c13
+	# CC: pre call
+	cmove $c19, $c11
+	cshrink $c11, $c11, 0
+	cuninit $c11, $c11
 	cjalr	$c12, $c17
 	nop
-	clc	$c1, $zero, 32($c11)    # 32-byte Folded Reload
+	# CC: post call
+	cmove $c11, $c19
+	clc	$c1, $zero, 0($c11)    # 32-byte Folded Reload
 	clw	$1, $zero, 0($c1)
 	dsll	$1, $1, 2
-	clc	$c2, $zero, 64($c11)    # 32-byte Folded Reload
-	csw	$2, $1, 0($c2)
+	clc	$c2, $zero, 32($c11)    # 32-byte Folded Reload
+	cincoffset $c2, $c2, $1
+	cincoffset $c2, $c2, 4 
+	ucsw	$c2, $2, 0($c2)
 	b	.LBB3_4
 	nop
 .LBB3_4:                                # %for.inc
                                         #   in Loop: Header=BB3_1 Depth=1
-	clc	$c1, $zero, 32($c11)    # 32-byte Folded Reload
+	clc	$c1, $zero, 0($c11)    # 32-byte Folded Reload
 	clw	$1, $zero, 0($c1)
 	addiu	$1, $1, 1
-	csw	$1, $zero, 0($c1)
+	cincoffset $c1, $c1, 4
+	ucsw	$c1, $1, 0($c1)
 	b	.LBB3_1
 	nop
 .LBB3_5:                                # %for.end
-	clc	$c1, $zero, 96($c11)    # 32-byte Folded Reload
+	clc	$c1, $zero, 64($c11)    # 32-byte Folded Reload
 	clw	$4, $zero, 0($c1)
-	clc	$c2, $zero, 128($c11)   # 32-byte Folded Reload
+	clc	$c2, $zero, 96($c11)   # 32-byte Folded Reload
 	clcbi	$c12, %capcall20(sum)($c2)
-	clc	$c3, $zero, 64($c11)    # 32-byte Folded Reload
+	clc	$c3, $zero, 32($c11)    # 32-byte Folded Reload
 	cgetnull	$c13
+	# CC: pre call
+	cmove $c20, $c11
+	cshrink $c11, $c11, 0
+	cuninit $c11, $c11
 	cjalr	$c12, $c17
 	nop
+	# CC: post call
+	cmove $c11, $c20
 	sll	$2, $2, 0
-	clc	$c17, $zero, 192($c11)  # 32-byte Folded Reload
-	cincoffset	$c11, $c11, 224
+	clc	$c17, $zero, 160($c11)  # 32-byte Folded Reload
 	cjr	$c17
 	nop
 	.set	at
@@ -341,8 +364,18 @@ test:                                   # @test
 	clcbi	$c12, %capcall20(sumFactorials)($c1)
 	cgetnull	$c13
 	csw	$2, $zero, 24($c11)     # 4-byte Folded Spill
+
+	# CC: pre call
+	cmove $c18, $c11
+	cshrink $c11, $c11, 0
+	cuninit $c11, $c11
+	
 	cjalr	$c12, $c17
 	nop
+
+	# CC: post call
+	cmove $c11, $c18
+
 	sll	$2, $2, 0
 	clc	$c17, $zero, 32($c11)   # 32-byte Folded Reload
 	cincoffset	$c11, $c11, 64
